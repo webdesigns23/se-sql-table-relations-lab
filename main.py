1c947cc68668333087a132159c50ea1f3d305beb
Laura Berge <lauraeberge@gmail.com>
2025-04-23 22:19:41 -0500

    convert lab from notebook to py file

---
+++ main.py
@@ -0,0 +1,52 @@
+# STEP 0
+
+# SQL Library and Pandas Library
+import sqlite3
+import pandas as pd
+
+# Connect to the database
+conn = sqlite3.connect('data.sqlite')
+
+pd.read_sql("""SELECT * FROM sqlite_master""", conn)
+
+# STEP 1
+# Replace None with your code
+df_boston = None
+
+# STEP 2
+# Replace None with your code
+df_zero_emp = None
+
+# STEP 3
+# Replace None with your code
+df_employee = None
+
+# STEP 4
+# Replace None with your code
+df_contacts = None
+
+# STEP 5
+# Replace None with your code
+df_payment = None
+
+# STEP 6
+# Replace None with your code
+df_credit = None
+
+# STEP 7
+# Replace None with your code
+df_product_sold = None
+
+# STEP 8
+# Replace None with your code
+df_total_customers = None
+
+# STEP 9
+# Replace None with your code
+df_customers = None
+
+# STEP 10
+# Replace None with your code
+df_under_20 = None
+
+conn.close()
